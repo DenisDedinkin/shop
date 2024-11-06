08bc8a6d6932f94eeb0e53feb8bd52513f183574
Denis <you@example.com>
2024-11-06 17:22:48 +0300

    refactor: change search (Full text search)

--- shop/goods/utils.py
+++ shop/goods/utils.py
@@ -1,4 +1,6 @@
-from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchRank, SearchQuery
+
+# from django.db.models import Q
 
 from goods.models import Product
 
@@ -7,13 +9,20 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Product.objects.filter(id=int(query))
 
-    keyword = [word for word in query.split() if len(word) > 2]
+    vector = SearchVector('name', 'description')
+    query = SearchQuery(query)
 
-    q_objects = Q()
+    result = Product.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
 
-    for token in keyword:
-        q_objects |= Q(description__icontains=token)    # Поиск в описании
-        q_objects |= Q(name__icontains=token)           # Поиск в названии
+    return result
 
-    return Product.objects.filter(q_objects)
+    # keyword = [word for word in query.split() if len(word) > 2]
+    #
+    # q_objects = Q()
+    #
+    # for token in keyword:
+    #     q_objects |= Q(description__icontains=token)    # Поиск в описании
+    #     q_objects |= Q(name__icontains=token)           # Поиск в названии
+    #
+    # return Product.objects.filter(q_objects)
 
